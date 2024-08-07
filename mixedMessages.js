a343b6749922f5c3580a6baf7ab8a2ac65b6f114
Shaun Watkins <swatkins229@outlook.com>
2024-08-07 13:56:19 +0100

    Update mixedMessages.js
    - Add function to hardcoded quotes
    - Add function to  API quotes
    - Generate random quote message

--- mixedMessages.js
+++ mixedMessages.js
@@ -8,17 +8,40 @@ const quotes = [
     "Every morning in Africa, a gazelle wakes up. It knows it must run faster than the fastest lion, or it will be killed. Every morning a lion wakes up, It knowsit must outrun the slowest gazelle, or it will starve to death. It doesn't matter whether you are a lion or a gazelle. When the sun comes up, you better start running."
 ];
 
-// Generate a random index
+// Funtion to get random quote from hard-coded array.
 
-const randomIndex = Math.floor(Math.random() * quotes.length);
+const getRandomQuote = quotesArray => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  } 
 
-// Getting a random quote
+// function to fetch a random quote from Quotable API.
 
-const randomQuote = quotes [randomIndex];
+const getQuotableQuote = async () => {
+    try {
+        const response = await
+        fetch('https://api.quotable.io/random');
+        const data = await response.json();
 
-// Display the quote on your page
+        const quote = data.content;
+        const author = data.author || 'Anonymus';
 
-console.log(randomQuote);
+        return `'${quote}' - ${author}`;
+    } catch (error) {
+        console.error('Error fetching quote:' , error);
+        return 'An error occured while fetching the quote';
+    }  
+};
 
+// Decide which method to use to get a quote.
 
+const useHardcodedQuotes = Math.random() < 0.5; // 50% chance
 
+if (useHardcodedQuotes) {
+    const randomQuote = getRandomQuote();
+    console.log(randomQuote);
+} else {
+    getQuotableQuote().then ((quote) => {
+        console.log(quote);
+    });
+}
